522683fbb9972069ba198b2b7867a58bc9e56783
stephen <skaranja654@gmail.com>
2023-11-02 12:48:57 +0300

    :memo: feat: imported the image path rather than explicitly assigning it to a variable

--- client/src/features/Nav.js
+++ client/src/features/Nav.js
@@ -1,11 +1,11 @@
 // This is a feature that is reused in all pages and therefore, in reference to the MVVM architecture, this acts as the model where the data is declared into variables.
 import React from 'react';
 import '/home/steve/Italanta/Elewa-Test/client/src/styles/Nav.css'  //this links the component and the css file
+import logo from '/home/steve/Italanta/Elewa-Test/client/src/resources/logo/Logo_White_arvoyx.svg'; //This is the path to the logo image being assigned a variable.
 
 function Nav() {
 
     // these are the values that are to be rendered on the view model
-    const logo = "client/src/resources/logo/Logo_White_arvoyx.svg"; //This is the path to the logo image being assigned a variable.
     const logoAlt = "logo"
     const home = "Home";
     const aboutUs = "About Us";
@@ -18,7 +18,7 @@ function Nav() {
 
 
     return(
-        <div>
+        <div className="nav-body">
             <img className="logo" src={logo} alt={logoAlt}/>
             <ul className="nav-links">
                 <li>{home}</li>
